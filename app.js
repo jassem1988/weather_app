baeb31a2d79b5e4bf37bb0e6279d247bd6806362
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-23 13:04:14 +0300

    add options to request fn

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require("request");
+
+request({
+  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=1301%20lombard%20street%20philadelphia',
+  json: true
+}, (error, response, body) => {
+
+});
