d5800ab40ad1706dfaa6b07209b35f51a9c2c068
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-23 13:13:23 +0300

    add JSON.stringify

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body) => {
-
+  console.log(JSON.stringify(body, undefined, 2));
 });
