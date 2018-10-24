8ad09756704ce9791f4b25cf07e700ee4c3ce3db
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-24 08:59:23 +0300

    print error

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require("request");
 
 request({
-  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=1301%20lombard%20street%20philadelphia',
+  url: 'http://www.mapuestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2));
+  console.log(JSON.stringify(error, undefined, 2));
 });
