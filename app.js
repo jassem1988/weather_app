c608853ed8637e3dd5687edde33b7becd13568de
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-24 09:02:39 +0300

    fix error in url and print error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 request({
-  url: 'http://www.mapuestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=1301%20lombard%20street%20philadelphia',
+  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body) => {
   console.log(JSON.stringify(error, undefined, 2));
