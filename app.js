6833125c8c2b5cdd80f1e2e55c1568380ed1ca5a
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-25 08:50:02 +0300

    add error handling

--- app.js
+++ app.js
@@ -22,8 +22,11 @@ request({
 }, (error, response, body) => {
   if(error) {
     console.log('Unable to connect to MapQuest servers');
+  } else if(body.results[0].locations[0].geocodeQualityCode === 'A1XAX') {
+    console.log('Unable to find the Address');
+  } else if(body.results[0].locations[0].geocodeQualityCode !== 'A1XAX') {
+    console.log(`Address: ${body.results[0].providedLocation.location}`);
+    console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
+    console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
   }
-  console.log(`Address: ${body.results[0].providedLocation.location}`);
-  console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
-  console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
 });
