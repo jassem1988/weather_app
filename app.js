248eebef47ab85327e696fba69935500c5ec8ef3
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:17:43 +0300

    refactor request fn to geocode.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require("request"),
         yargs = require('yargs');
+        geocode = require('./geocode/geocode.js');
 
 const argv = yargs
   .options({
@@ -14,19 +15,22 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodeAddress = encodeURIComponent(argv.address); //or .a 'as the alias'
+geocode.geocodeAddress(argv.address);
 
-request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=${encodeAddress}`,
-  json: true
-}, (error, response, body) => {
-  if(error) {
-    console.log('Unable to connect to MapQuest servers');
-  } else if(body.results[0].locations[0].geocodeQualityCode === 'A1XAX') {
-    console.log('Unable to find the Address');
-  } else if(body.results[0].locations[0].geocodeQualityCode !== 'A1XAX') {
-    console.log(`Address: ${body.results[0].providedLocation.location}`);
-    console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
-  }
-});
+// var encodeAddress = encodeURIComponent(argv.address); //or .a 'as the alias'
+
+
+// request({
+//   url: `http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=${encodeAddress}`,
+//   json: true
+// }, (error, response, body) => {
+//   if(error) {
+//     console.log('Unable to connect to MapQuest servers');
+//   } else if(body.results[0].locations[0].geocodeQualityCode === 'A1XAX') {
+//     console.log('Unable to find the Address');
+//   } else if(body.results[0].locations[0].geocodeQualityCode !== 'A1XAX') {
+//     console.log(`Address: ${body.results[0].providedLocation.location}`);
+//     console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
+//     console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
+//   }
+// });
