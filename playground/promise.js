943cc851343556e562f2ab161c4b32ac236e4a86
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    call asyncAdd fn

--- playground/promise.js
+++ playground/promise.js
@@ -1,6 +1,6 @@
 var asyncAdd = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(()= {
+    setTimeout(() => {
       if(typeof a === 'number' && typeof b === 'number') {
         resolve(a + b);
       } else {
@@ -10,15 +10,21 @@ var asyncAdd = () => {
   });
 };
 
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Hey. It worked!'); //you can only resolve OR reject once
-    // reject('Unable to fulfill the promise');
-  }, 2500);
+asyncAdd(5,7).then((res) => {
+  console.log('Result: ' ,res);
+}, (errorMessage) => {
+  console.log(errorMessage);
 });
 
-somePromise.then((message) => {
-  console.log('Success: ', message);
-}, (errorMessage) => {
-  console.log('Error: ', errorMessage);
-});
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Hey. It worked!'); //you can only resolve OR reject once
+//     // reject('Unable to fulfill the promise');
+//   }, 2500);
+// });
+//
+// somePromise.then((message) => {
+//   console.log('Success: ', message);
+// }, (errorMessage) => {
+//   console.log('Error: ', errorMessage);
+// });
