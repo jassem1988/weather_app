773f533d2500dc9549d9c81b1dec2c2185f87ed1
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:18:22 +0300

    clear up app.js file

--- app.js
+++ app.js
@@ -16,21 +16,3 @@ const argv = yargs
   .argv;
 
 geocode.geocodeAddress(argv.address);
-
-// var encodeAddress = encodeURIComponent(argv.address); //or .a 'as the alias'
-
-
-// request({
-//   url: `http://www.mapquestapi.com/geocoding/v1/address?key=43K4lEdm8kPrYnWGALeLEJDE1ZGKMP50&location=${encodeAddress}`,
-//   json: true
-// }, (error, response, body) => {
-//   if(error) {
-//     console.log('Unable to connect to MapQuest servers');
-//   } else if(body.results[0].locations[0].geocodeQualityCode === 'A1XAX') {
-//     console.log('Unable to find the Address');
-//   } else if(body.results[0].locations[0].geocodeQualityCode !== 'A1XAX') {
-//     console.log(`Address: ${body.results[0].providedLocation.location}`);
-//     console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
-//     console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
-//   }
-// });
