f10be409196be28a9552ef3d9e296d1dd249a7d2
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    add comments

--- playground/promise.js
+++ playground/promise.js
@@ -1,6 +1,6 @@
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    //resolve('Hey. It worked!');
+    //resolve('Hey. It worked!'); //you can only resolve OR reject once
     reject('Unable to fulfill the promise');
   }, 2500);
 });
