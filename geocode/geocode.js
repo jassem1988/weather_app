40eb9d0024b3a26edbe2b51635d2c1d122ed4ed5
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    add callback to geocodeAddress fn

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-var geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
 
   var encodeAddress = encodeURIComponent(address); //or .a 'as the alias'
 
@@ -9,13 +9,15 @@ var geocodeAddress = (address) => {
     json: true
   }, (error, response, body) => {
     if(error) {
-      console.log('Unable to connect to MapQuest servers');
+      callback('Unable to connect to MapQuest servers'));
     } else if(body.results[0].locations[0].geocodeQualityCode === 'A1XAX') {
-      console.log('Unable to find the Address');
+      callback('Unable to find the Address');
     } else if(body.results[0].locations[0].geocodeQualityCode !== 'A1XAX') {
-      console.log(`Address: ${body.results[0].providedLocation.location}`);
-      console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
-      console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
+      callback(undefined, {
+        address: body.results[0].providedLocation.location,
+        latitude: body.results[0].locations[0].displayLatLng.lat,
+        longitude: body.results[0].locations[0].displayLatLng.lng
+      });
     }
   });
 };
