17a5e15d454192c4c7f678100ed316f852f564d2
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    add reject to somePromise

--- playground/promise.js
+++ playground/promise.js
@@ -1,6 +1,7 @@
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve('Hey. It worked!');
+    //resolve('Hey. It worked!');
+    reject('Unable to fulfill the promise');
   }, 2500);
 });
 
