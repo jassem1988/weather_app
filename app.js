b2cbaaf921108c4dbb3521e41856a01e19fc5f59
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-28 11:07:20 +0300

    add callback to getWather fn

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// const   yargs = require('yargs'),
-//         geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weathe for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
+const   yargs = require('yargs'),
+        geocode = require('./geocode/geocode'),
+        weather = require('./weather/weather');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weathe for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
 // geocode.geocodeAddress(argv.address, (errorMessage, results) => {
 //   if(errorMessage) {
 //     console.log(errorMessage);
@@ -22,20 +23,10 @@
 //   }
 // });
 
-
-// e9dfa3a5dbd649460d5a0bed5bb9bc27
-
-const request = require("request");
-
-request({
-  url: 'https://api.darksky.net/forecast/e9dfa3a5dbd649460d5a0bed5bb9bc27/36.697079,-76.963451',
-  json: true
-}, (error, response, body) => {
-    if(error) {
-      console.log("Unable to connect to Forcast.io servers");
-    } else if(!error & response.statusCode === 200) {
-      console.log(body.currently.temperature);
-    } else {
-      console.log('Unable to fetch weather');
-    }
+weather.getWeather(36.697079, -76.963451, (errorMessage, results) => {
+  if(errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
