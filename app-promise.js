20cf4f65f144fe0d5112eed533137c4915f6ac28
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-30 11:23:34 +0300

    duplicate app-promise.js file

---
+++ app-promise.js
@@ -0,0 +1,15 @@
+const   yargs = require('yargs'),
+
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weathe for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
